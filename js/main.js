bbe1af42db85921ee667f334b09335f22a6b3588
bigmoumou <o1r2g3a4n5i6z7e8@gmail.com>
2018-01-15 21:44:58 +0800

    add html demo

--- js/main.js
+++ js/main.js
@@ -284,5 +284,12 @@ require(["finalseg","data/dictionary"], function(finalseg, dictionary) {
     // initialize when the file loads (no lazy-loading yet):
     initialize();
 
-    console.log(cut("我爸新学会了一项解决日常烦闷的活动，就是把以前的照片抱回办公室扫描保存，弄成电子版的。更无法接受的是，还居然放到网上来，时不时给我两张。这些积尘的化石居然突然重现，简直是招架不住。这个怀旧的阀门一旦打开，那就直到意识模糊都没停下来。", true));
+
+    document.querySelector('#button').onclick = function(){
+        var input_data = document.querySelector("#input_demo").value;
+        var demo = cut(input_data, true);
+
+        console.log(demo);
+        document.querySelector('#output_demo').value = demo;
+    };
 });
